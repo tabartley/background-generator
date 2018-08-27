de130d9e38003b2269fd657e5520865a7c6842d3
tabartley <tamyra.bartley@gmail.com>
2018-08-26 22:15:49 -0400

    Resized input field.  "Let Me Pick" Button now working.
    Need to get the copy to clipboard function working and have the direction of the gradient appear when clicked.  Currently "to right" appears with every direction.

--- script.js
+++ script.js
@@ -6,24 +6,13 @@ var topp = document.getElementById("topp");
 var right = document.getElementById("right");
 var bottom = document.getElementById("bottom");
 var left = document.getElementById("left");
+var random = document.getElementById("random");
 
 
 
-
-function setGradient(){
-  body.style.background =
-      "linear-gradient(to right, "
-      + color1.value
-      + ", "
-      + color2.value
-      + ")";
-
-      css.textContent = body.style.background + ";";
-
-  }
 
   // function setGradient(){
-  //   const aVariable =
+  //   var aVariable =
   //       "linear-gradient(to right, "
   //       + color1.value
   //       + ", "
@@ -33,59 +22,99 @@ function setGradient(){
   //       css.textContent = aVariable + ";";
   //       body.syle.bacground = css.textContent;
   //       console.log(css.textContent);
-    // }
+  //   }
+
+
+  function setGradient(){
+    body.style.background =
+        "linear-gradient(to right, "
+    
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
+
+        css.textContent = body.style.background + ";";
+
+    }
+
+
 
 
 
   function setDirectionTop(){
     body.style.background =
-        "linear-gradient(to top, "
+        "linear-gradient(to bottom, "
+
         + color1.value
         + ", "
         + color2.value
         + ")";
 
-        css.textContent = body.style.background + ";";
+
     }
+
+
+
+
+
 
 
     function setDirectionRight(){
       body.style.background =
-        "linear-gradient(to right, "
-        + color1.value
-        + ", "
-        + color2.value
-        + ")";
+          "linear-gradient(to left, "
 
-        css.textContent = body.style.background + ";";
+          + color1.value
+          + ", "
+          + color2.value
+          + ")";
+
       }
 
 
       function setDirectionBottom(){
         body.style.background =
-        "linear-gradient(to bottom, "
-        + color1.value
-        + ", "
-        + color2.value
-        + ")";
+            "linear-gradient(to top, "
 
-        css.textContent = body.style.background + ";";
+            + color1.value
+            + ", "
+            + color2.value
+            + ")";
+
+
         }
-
 
 
 
         function setDirectionLeft(){
           body.style.background =
-        "linear-gradient(to left, "
-        + color1.value
-        + ", "
-        + color2.value
-        + ")";
+              "linear-gradient(to right, "
 
-        css.textContent = body.style.background + ";";
+              + color1.value
+              + ", "
+              + color2.value
+              + ")";
+
           }
 
+
+
+
+    function setRandomGradient(){
+    	var rcolor1 = "#"+Math.floor(Math.random()*16777215).toString(16);
+    	var rcolor2 = "#"+Math.floor(Math.random()*16777215).toString(16);
+
+    	color1.value = rcolor1;
+    	color2.value = rcolor2;
+    	body.style.background = "linear-gradient(to right,"
+      + rcolor1
+    	+ ","
+    	+ rcolor2
+    	+ ")";
+
+    	css.textContent = body.style.background + ";";
+
+    }
 
 
 
@@ -104,3 +133,7 @@ right.addEventListener("click", setDirectionRight);
 bottom.addEventListener("click", setDirectionBottom);
 
 left.addEventListener("click", setDirectionLeft);
+
+random.addEventListener("click", setRandomGradient);
+
+setRandomGradient();
